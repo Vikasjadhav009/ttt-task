5d474c7cfdaf86571da93ac199735e1ce0e02a73
Viki jadhav <provikasjadhav23@gmail.com>
2017-12-30 00:02:50 +0530

    ttt first commit

---
+++ app.js
@@ -0,0 +1,115 @@
+//use express 
+var express = require('express');
+var app = express();
+
+//use request module to fetch file from remote server
+var request = require('request');
+
+//check for enviroment veriable otherwise set default port 3000
+var port = process.env.PORT || 3000;
+ 
+//use ejs for server side templating
+app.set('view engine','ejs');
+
+//use this public directory to store my angular files
+app.use('/assets',express.static(__dirname+'/public'));
+
+//render default index.ejs page
+app.get('/',function(req,res){
+
+	res.render('index');
+});
+
+//fetched text file from remote server
+request('http://terriblytinytales.com/test.txt',function(err,res,body){
+
+	 //use split method to create array of all words in text file
+	 var fileArr = body.split(/\W+/);
+
+	 var unique = [];
+	 var repeat = [];
+ 	 var topN = [];
+
+ 	fileArr.forEach(function(cur,index){
+
+ 			//loop through fileArr to find out unique words and create new array
+
+ 			if(unique.indexOf(cur) === -1){
+
+ 		 			unique.push(cur);
+ 		 			
+ 		 		}
+ 		
+
+
+  	});
+
+ 	//compare this 2 array to find out no of occurrence and push into new array repeat
+ 	unique.forEach(function(cur,index){
+ 	var count = 0;
+
+ 		for(var i=0; i<fileArr.length; i++){
+
+ 			if(cur === fileArr[i] ){
+
+ 				count++;
+ 				
+
+ 			}
+
+ 		}
+ 		repeat.push(count);
+
+ });
+
+//sort repeat array to find out rank
+var sortArr = repeat.slice(0);
+
+ sortArr.sort(function(a,b){
+
+ 	return b-a;
+ });
+
+ //stored index of sortArr in new array 
+		sortArr.forEach(function(cur,index){
+
+			if(sortArr[index] === sortArr[index-1]){
+
+				return;
+			}
+
+			for(var i = 0;i<repeat.length;i++){
+
+		 		if(repeat[i] === cur){
+
+		 			topN.push(i)
+		 		}
+			 }
+
+
+		});
+
+
+	//create object of data which contains all array	
+		var data = {
+			unique:unique,
+			sortArr:sortArr,
+			topN:topN,
+			fileArr:fileArr
+		}
+
+	//send this object as JSON to frontend (AngularJS) as http req to display result
+	app.get('/resultapi',function(req,res){
+
+			res.json(data);
+
+	});
+
+
+
+
+});
+
+
+
+app.listen(port);
---
+++ public/js/main.js
@@ -0,0 +1,102 @@
+angular.module('testapp',[]);
+
+angular.module('testapp').controller('MainController',mainCtrl);
+
+
+function mainCtrl($scope,$http) {
+	var test;
+
+	function action(){
+		
+
+
+		if($('.tb').children().length > 0)
+		{
+			$(".table").empty();
+			$("#ttt").empty();
+		}
+
+		test = Number(document.getElementById('noInput').value);
+
+		if(test !== '' && !isNaN(test) && test > 0){
+				
+					//use angular http get method to catch api send by nodeJS server
+					
+					$http.get('http://127.0.0.1:3000/resultapi').then(function(res){
+
+						
+						 if(test > res.data.unique.length){
+
+						 	alert(`max limit is ${res.data.unique.length} ,therefor showing result for ${res.data.unique.length} words`);
+						 	test = res.data.unique.length;
+
+
+						 }
+
+						var note = 'Data fetched from <kbd>http://terriblytinytales.com/test.txt</kbd> ,Showing Result for Input &nbsp;<kbd> '+test+' </kbd>';
+						document.querySelector('#ttt').insertAdjacentHTML('beforeend',note);
+						$('#ttt').show();
+
+						var htm = '<thead><tr><th>Rank</th><th>Word</th><th>Repeat Count</th></tr></thead>' 
+						document.querySelector('.table').insertAdjacentHTML('beforeend',htm);
+
+						for(i=0;i<test;i++){
+
+
+						var  html = '<tbody class="tb"><tr><td>%rank</td><td>%word</td><td>%repeatcount</td></tr></tbody>';
+			
+						var newHtml = html.replace('%rank',i+1);
+						var newHtml = newHtml.replace('%word',res.data.unique[res.data.topN[i]]);
+						var newHtml= newHtml.replace('%repeatcount',res.data.sortArr[i]);
+
+						document.querySelector('.table').insertAdjacentHTML('beforeend',newHtml);		
+						
+
+				}
+			});
+
+
+		}
+		else{
+			alert('Please Enter Valid Input!!!!');
+			clearField();
+		}
+		
+	}	
+
+	function clearField(){
+
+		document.getElementById('noInput').value = '';
+		document.getElementById('noInput').focus();
+	}
+
+
+	document.getElementById('subBtn').addEventListener('click',function(){
+			
+			action();
+			clearField();
+	});
+
+	document.addEventListener('keypress',function(event){
+
+
+			if(event.keyCode === 13){ 
+
+					action();
+					clearField();
+				
+			}
+
+	});
+
+
+	document.getElementById('trybtn').addEventListener('click',function(){
+		$(".table").empty();
+		$("#ttt").empty();
+		clearField();
+		$('#ttt').hide();			
+
+	});
+
+	
+}
